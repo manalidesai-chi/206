df0dcd29cdfe26fd53192e993a74c6334ebd73bf
Manali Desai <schrodingerscat1901@gmail.com>
2017-11-07 16:53:12 -0500

    Delete twitter_info.py

--- hw8/twitter_info.py
+++
@@ -1,9 +0,0 @@
-#twitter info
-
-
-
-
-consumer_key = "Gw4VuhjX5Yjn767DEXbq4WHk6" 
-consumer_secret = "AdXmR6p16UmfOowLqoGpqZEbfnQX8MpauAlu7avVj9PvCDTuy4"
-access_token = "921136458832007168-98qwlAxZKhjov8WOSjrQAnxHu6feUqa"
-access_token_secret = "LzrcBnwb8arLTtWTYvi6YiWGJsYIp4ECQnXKur9wWLTlH"
